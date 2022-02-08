734751c0c4d3e5e0aed2975a76b5df66a983dff5
DUBOSCLARD LÉO <leo.dubosclard@epitech.eu>
2022-02-08 15:58:19 +0100

    .env created

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 const express = require('express');
+const dotenv = require('dotenv');
 const app = express();
-const port = 3000;
+dotenv.config();
 
-app.listen(port, () => {
-    console.log('App listenting on {port}')
+app.listen(process.env.PORT, () => {
+    console.log(`App listenting on ${process.env.PORT}`);
 });
