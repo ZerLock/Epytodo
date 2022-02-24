c7feb2cd74f26be5921776f02ff89c196468b8df
DUBOSCLARD LÉO <leo.dubosclard@epitech.eu>
2022-02-24 09:32:28 +0100

    FOREIGN KEY

--- epytodo.sql
+++ epytodo.sql
@@ -1,16 +1,16 @@
 CREATE DATABASE epytodo;
 
-use epytodo;
+USE epytodo;
 
 CREATE TABLE user
 (
-    id BIGINT NOT NULL AUTO_INCREMENT,
+    id BIGINT unsigned NOT NULL AUTO_INCREMENT,
     email VARCHAR(255) UNIQUE,
     password VARCHAR(255) NOT NULL,
     name VARCHAR(255) NOT NULL,
     firstname VARCHAR(255) NOT NULL,
     create_at DATETIME NOT NULL DEFAULT NOW(),
-    CONSTRAINT id PRIMARY KEY (id)
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE todo
@@ -22,5 +22,6 @@ CREATE TABLE todo
     due_time DATETIME NOT NULL,
     status VARCHAR(255) NOT NULL,
     user_id BIGINT unsigned NOT NULL,
-    CONSTRAINT t_id PRIMARY KEY (id)
+    PRIMARY KEY (id),
+    FOREIGN KEY (user_id) REFERENCES user(id) ON DELETE CASCADE
 );
